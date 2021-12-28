e61f0a560293f9db6146bc9349e99b4c2a2e2e57
Anoop <Anoop@NTP-LAP-226.chn.nestgroup.net>
2021-12-28 17:08:16 +0530

    first commit

---
+++ src/test/java/com/bank/transactionstream/TransactionstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.transactionstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
