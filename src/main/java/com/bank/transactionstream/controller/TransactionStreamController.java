c8094ea6062cc565c9ba1e51575cf003fe75972c
Anoop <Anoop@NTP-LAP-226.chn.nestgroup.net>
2022-01-12 21:57:25 +0530

    checked functionalities

---
+++ src/main/java/com/bank/transactionstream/controller/TransactionStreamController.java
@@ -0,0 +1,15 @@
+package com.bank.transactionstream.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TransactionStreamController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("worked");
+
+	}
+
+}
